fcbb1f392ec16360e2843b64c5ef3a5c6435882d
Sergio R <sergio.vns.19@gmail.com>
2022-02-17 10:18:34 -0500

    Clase 4

---
+++ Clase 4/js/main.js
@@ -0,0 +1,10 @@
+// function numeroAlCuadrado ( numero) {
+//     let result = numero * numero
+//     return result
+// }
+
+// let result = numeroAlCuadrado(4)
+// console.log(result);
+
+
+//Funciones flecha ejemplos
